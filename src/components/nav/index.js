6cdcbd71062ab49ae0516b27c45f425022eefb71
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-27 17:10:08 -0500

    create nav and add flexbox

---
+++ src/components/nav/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Nav () {
+    
+    return (
+        <header>
+            <nav>
+                <h1>Bleakney Bissett</h1>
+                <div className="navlinks-container">
+                    <span className="nav-link navlink-margin">WORK</span>
+                    <span className="nav-link navlink-margin">ABOUT</span>
+                    <span className="nav-link">CONTACT</span>
+                </div>
+            </nav>
+        </header>
+    )
+};
+
+export default Nav;
