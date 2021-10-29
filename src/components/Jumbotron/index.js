4489d847d4e9a67bc427e64d612c0c7acb290523
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-28 21:13:36 -0500

    create homepage jumbotron component and import

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Jumbotron () {
+
+    return (
+        <section className="jumbotron">
+            
+        </section>
+    )
+};
+
+export default Jumbotron;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import Jumbotron from '../../components/Jumbotron';
 
 function Home () {
     
     return (
-        <div>
-
-        </div>
+        <Jumbotron />
     )
 };
 
