31badfaf794e19cb6b990fd91f3415475dda6f06
Bleakney Bissett <bleakneybissett@gmail.com>
2021-10-29 14:30:49 -0500

    create animation for homepage

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
+import JumboTronTrail from '../JumbotronTrail';
 
-function Jumbotron () {
+export default function Jumbotron() {
+  const text = [
+    "hi there. welcome to my website.",
+    "i'm a former advertising creative", 
+    "turned UI/UX engineer.",
+    "here's some stuff i made.",
+    "i hope you like it.",
+  ];
+  //   const props = useSpring({
+  //     from: { opacity: 0, transform: 'translateX(0)' },
+  //     to: { opacity: 1, transform: 'translateX(30px)' },
+  //     delay: 500,
+  //   });
+  const [open, setOpen] = useState(true);
+  return (
+    <section className={`jumbotron-container`} onClick={() => setOpen(state => !state)}>
+        <JumboTronTrail items={text} open={open} />
 
-    return (
-        <section className="jumbotron">
-            
-        </section>
-    )
+      {/* {text.map((text, i) => (
+        <animated.h1 key={`text-${i}`} style={props}>
+          {text}
+        </animated.h1>
+      ))} */}
+    </section>
+  );
 };
-
-export default Jumbotron;
---
+++ src/components/JumbotronTrail/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useTrail, animated, useSpring } from "react-spring";
+import { Link } from "react-router-dom";
+
+export default function Trail(props) {
+  const { open, items } = props;
+
+  const trail = useTrail(items.length, {
+    config: { mass: 10, tension: 210, friction: 50 },
+    opacity: open ? 1 : 0,
+    y: open ? 0 : 20,
+    x: open ? 0 : 20,
+    height: open ? "100vh" : "0vh",
+    width: open ? "80vw" : "0vw",
+    from: { opacity: 0, y: 20, x: 20, height: "0vh", width: "0vw" },
+  });
+
+  const spring = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 4000,
+  });
+  return (
+    <div className="trail-container">
+      <div>
+        {trail.map(({ height, width, ...style }, index) => (
+          <animated.div key={index} className="trail-text" style={style}>
+            <animated.div style={{ height, width }}>
+              {items[index]}
+            </animated.div>
+          </animated.div>
+        ))}
+      </div>
+      <Link to="/work">
+        {" "}
+        <animated.span style={spring} className="pointer-symbol">
+          &#9758;
+        </animated.span>{" "}
+      </Link>
+    </div>
+  );
+}
