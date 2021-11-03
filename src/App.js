52f2d0ca1e0fd9facef22f6f3d2421cbc4c70669
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-03 17:54:35 -0500

    temporarily remove contact page, add state to show active page in nav

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FOG from "vanta/dist/vanta.fog.min";
 // import stylesheet and fonts
-import './styles.scss';
-import WebFont from 'webfontloader';
+import "./styles.scss";
+import WebFont from "webfontloader";
 //import components
-import Nav from './components/Nav';
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
-import Work from './pages/Work';
+import Work from "./pages/Work";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 function App() {
+  // set up nav state
+  const [aboutSelected, setAboutSelected] = useState(false);
+  const [workSelected, setWorkSelected] = useState(false);
+
   // import google fonts
   useEffect(() => {
     WebFont.load({
       google: {
-        families: ['Playfair Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900', 'Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900']
-      }
-    })
-  })
+        families: [
+          "Playfair Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900",
+          "Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900",
+        ],
+      },
+    });
+  });
   const [vantaEffect, setVantaEffect] = useState(0);
   // const myRef = useRef(null);
   useEffect(() => {
@@ -43,17 +50,21 @@ function App() {
 
   return (
     <Router>
-  <div className="vanta-container">
-    <Nav />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/work" component={Work} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/contact" component={Contact} />
-    </Switch>
-
-  </div>
-  </Router>
+      <div className="vanta-container">
+        <Nav
+          aboutSelected={aboutSelected}
+          setAboutSelected={setAboutSelected}
+          workSelected={workSelected}
+          setWorkSelected={setWorkSelected}
+        />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/about" component={About} />
+          {/* <Route exact path="/contact" component={Contact} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Nav () {
+function Nav (props) {
+    const {
+        aboutSelected,
+        setAboutSelected,
+        workSelected,
+        setWorkSelected
+    } = props;
     
     return (
         <header>
             <nav>
-               <Link to="/"> <h1>Bleakney Bissett</h1> </Link>
+               <Link to="/" onClick={() => {setWorkSelected(false); setAboutSelected(false);}}> <h1>Bleakney Bissett</h1> </Link>
                 <div className="navlinks-container">
-                   <Link to="/work"> <span className="nav-link navlink-margin">WORK</span> </Link>
-                   <Link to="/about"> <span className="nav-link navlink-margin">ABOUT</span> </Link>
-                    <Link to="/contact"> <span className="nav-link">CONTACT</span> </Link>
+                   <Link to="/work"> <span className={`nav-link navlink-margin ${workSelected && `navlink-active`}`} onClick={() => {setWorkSelected(true); setAboutSelected(false);}} >WORK</span> </Link>
+                   <span className="navlink-margin"> / </span>
+                   <Link to="/about"> <span className={`nav-link navlink-margin ${aboutSelected && `navlink-active`}`} onClick={() => {setAboutSelected(true); setWorkSelected(false);}} >ABOUT</span> </Link>
+                    {/* <Link to="/contact"> <span className="nav-link">CONTACT</span> </Link> */}
                 </div>
             </nav>
         </header>
