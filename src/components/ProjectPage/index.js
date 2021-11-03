388a2bdeca851d7381953a0776658d69936fec99
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-03 12:16:05 -0500

    create parallax layers for project images/titles

--- src/components/ProjectPage/index.js
+++ src/components/ProjectPage/index.js
@@ -5,19 +5,28 @@ import WorkDoodleSvg from '../WorkDoodleSvg';
 
 export default function ProjectPage ({ offset, gradient, onClick }) {
 
+    const projectImages = ['./images/free-maps.png', './images/movie-date.png', './images/broadcastr.png'];
+    const projectTitles = ['Free Maps', 'Movie Date', 'broadcastr'];
+
    
 return (
     <div className="project-page-container">
-        <ParallaxLayer offset={offset} speed={0.2}>
-            <div className="svg-container" >
-                <WorkDoodleSvg />
+        <ParallaxLayer offset={offset} speed={0.6}>
+            <div className="svg-container centered" >
+                <WorkDoodleSvg /> 
             </div>
             </ParallaxLayer>
-    {/* <ParallaxLayer>
-        <div className="project-title-container">
-        <h2>Free Maps</h2>
+<ParallaxLayer offset={offset}>
+    <div className="project-image-container centered">
+<img className="project-page-image" src={projectImages[offset]} />
+<div className="project-title-container">
+        <h1>{projectTitles[offset]}</h1>
         </div>
-    </ParallaxLayer> */}
+</div>
+</ParallaxLayer>
+    <ParallaxLayer offset={offset} speed={0.1}>
+       
+    </ParallaxLayer>
     </div>
 );
 };
