2e36b0bdf0c796a45d5794c4943c332539c66c61
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-03 17:54:13 -0500

    ESLint corrections, add alt to headshot img

--- src/components/AboutSection/index.js
+++ src/components/AboutSection/index.js
@@ -7,7 +7,7 @@ export default function AboutSection () {
             <h1>ABOUT ME</h1> 
             <div className="about-content">
             <div className="about-image-container">
-                <img src="./images/me.jpg" style={{"border-radius": "50%"}}/>
+                <img src="./images/me.jpg" alt="headshot" style={{"borderRadius": "50%"}}/>
                 <div className="social-links-container">
                 <a className="svg-link-container" href="https://github.com/bleakney"><img src="./images/svg/github.svg" alt="linkedIn" /></a>
                 <a className="svg-link-container" href="https://linkedin.com/in/bleakney-bissett"><img src="./images/svg/linkedin.svg" alt="linkedIn" /></a>
@@ -15,7 +15,7 @@ export default function AboutSection () {
                 </div>
             </div>
             <div style={{"width": "40%"}}>
-            <p><span className="paragraph-start">G</span>lad you asked. I'm a full-stack web developer experienced with the MERN stack. I received my B.S. in Advertising from the University of Texas at Austin in 2020. While there, I applied and was accepted to Texas Creative, a highly competitive portfolio program where I earned my chops at art direction and copywriting. I learned a <span style={{"font-style": "italic"}}>lot</span>, including the fact that I didn't particularly want to work in advertising. Sometime after that we had a pandemic, and I needed a hobby. Boredom and curiosity led to me start fiddling around with coding, and it turns out, I loved it. I went back to UT and enrolled in their 6-month Full-Stack Coding Bootcamp, graduating with a 97.2 average. I love design and I love writing code. If you're looking to hire someone who can do both, drop me a line.
+            <p><span className="paragraph-start">G</span>lad you asked. I'm a full-stack web developer experienced with the MERN stack. I received my B.S. in Advertising from the University of Texas at Austin in 2020. While there, I applied and was accepted to Texas Creative, a highly competitive portfolio program where I earned my chops at art direction and copywriting. I learned a <span style={{"fontStyle": "italic"}}>lot</span>, including the fact that I didn't particularly want to work in advertising. Sometime after that we had a pandemic, and I needed a hobby. Boredom and curiosity led to me start fiddling around with coding, and it turns out, I loved it. I went back to UT and enrolled in their 6-month Full-Stack Coding Bootcamp, graduating with a 97.2 average. I love design and I love writing code. If you're looking to hire someone who can do both, drop me a line.
 </p>
 </div>
 </div>
