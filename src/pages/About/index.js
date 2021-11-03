c22ad1e7cf25e8602143b4c38fc85889feea8d34
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-03 17:33:27 -0500

    create about section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import AboutSection from '../../components/AboutSection';
 
 function About () {
     
     return (
-        <div>
-this is the about page
-        </div>
+        <AboutSection />
     )
 };
 
