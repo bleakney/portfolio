3bf8020373e36a37c1adb2bc981adc26e2628ddd
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-08 17:27:42 -0600

    set up dynamic links to repos/deployments

--- src/components/WorkSection/index.js
+++ src/components/WorkSection/index.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 // import HorizontalScroll from 'react-scroll-horizontal';
 import ProjectPage from "../ProjectPage";
@@ -6,19 +6,27 @@ import ProjectPage from "../ProjectPage";
 function WorkSection() {
   const parallax = useRef(Parallax);
 
+  const githubLinks = ['https://github.com/bleakney/free-maps', 'https://github.com/danielolvera21/movie_date', 'https://github.com/rmwillow/theSocialScoop'];
+  const deploymentLinks = ['https://tranquil-garden-66594.herokuapp.com/', 'https://danielolvera21.github.io/movie_date/index.html', 'https://broadcastrss.herokuapp.com/' ]
+  const [index, setIndex] = useState(0);
+
   const scrollForward = (currentPageNumber) => {
     if (currentPageNumber < 2) {
+    setIndex(currentPageNumber + 1);
       parallax.current.scrollTo(currentPageNumber + 1 );
     } else {
+        setIndex(0);
         parallax.current.scrollTo(0);
     }
   };
 
   const scrollBackward = (currentPageNumber) => {
       if (currentPageNumber > 0) {
+          setIndex(currentPageNumber - 1);
         parallax.current.scrollTo(currentPageNumber - 1 );
       } else {
         parallax.current.scrollTo(2);
+        setIndex(2);
       }
   }
 
@@ -32,6 +40,10 @@ function WorkSection() {
             <h1 className="parallax-left-arrow" onClick={() => scrollBackward(parallax.current.offset)}>&#10094;</h1>
             <h1 className="parallax-right-arrow" onClick={() => scrollForward(parallax.current.offset)}>&#10095;</h1>
             </div>
+            <div className="project-links-container">
+          <a className="svg-link-container" href={githubLinks[index]}><img src="./images/svg/github.svg" alt="github repository" /></a>
+          <a className="svg-link-container" href={deploymentLinks[index]}><h4>view deployment</h4></a>
+          </div>
           </div>
         </ParallaxLayer>
         <ProjectPage offset={0} onClick={() => scrollForward(1)} />
