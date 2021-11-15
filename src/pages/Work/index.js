71c5aa5c4b9e2698a51b5a0ce26e87dd6436a40d
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-15 16:51:42 -0600

    add fade animation to work page

--- src/pages/Work/index.js
+++ src/pages/Work/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import WorkSection from '../../components/WorkSection';
+import { useSpring, animated } from 'react-spring';
 
 function Work () {
+    const fadeIn = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: 1},
+        delay: 500
+    });
+
     
     return (
+        <animated.div style={fadeIn}>
         <WorkSection />
+        </animated.div>
     )
 };
 
