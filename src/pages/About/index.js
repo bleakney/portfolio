f531989f25485a5c2dc87fe29eb24b083172c665
Bleakney Bissett <bleakneybissett@gmail.com>
2021-11-15 16:54:32 -0600

    add fade in animation to about page"

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import AboutSection from '../../components/AboutSection';
+import { useSpring, animated } from 'react-spring';
 
 function About () {
+
+    const fadeIn = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: 1},
+    });
     
     return (
+        <animated.div style={fadeIn}>
         <AboutSection />
+        </animated.div>
     )
 };
 
